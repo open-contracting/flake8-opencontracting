39d846a5928ef10655c15c3ad495704be5c359b6
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-01-07 00:22:45 -0500

    fix: Use flake8>=5 to avoid old plugin API that requires name, version

--- tests/test_module.py
+++ tests/test_module.py
@@ -7,8 +7,8 @@ def test_module(capsys):
     assert main((os.path.join("tests", "fixtures", "noqa.py"),)) == 1
 
     assert capsys.readouterr().out == """\
-tests/fixtures/noqa.py:11:27: FOC100 noqa: F401,E402
-tests/fixtures/noqa.py:18:6: FOC100 noqa: E201
+tests/fixtures/noqa.py:11:27: FOC100 noqa: F401,E402 isort:skip
+tests/fixtures/noqa.py:19:6: FOC100 noqa: E201
 """
 
     # Note: flake8 skips the bare "noqa".
