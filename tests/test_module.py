d918934e8ef204a98cb36374e5c6d07468e899ce
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-05 15:27:22 -0400

    chore: black

--- tests/test_module.py
+++ tests/test_module.py
@@ -6,9 +6,9 @@ from flake8.main.cli import main
 def test_module(capsys):
     assert main((os.path.join("tests", "fixtures", "noqa.py"),)) == 1
 
-    assert capsys.readouterr().out == f"""\
-tests{os.sep}fixtures{os.sep}noqa.py:11:27: FOC100 noqa: F401,E402 isort:skip
-tests{os.sep}fixtures{os.sep}noqa.py:19:6: FOC100 noqa: E201
-"""
+    assert (
+        capsys.readouterr().out == f"tests{os.sep}fixtures{os.sep}noqa.py:11:27: FOC100 noqa: F401,E402 isort:skip\n"
+        f"tests{os.sep}fixtures{os.sep}noqa.py:19:6: FOC100 noqa: E201\n"
+    )
 
     # Note: flake8 skips the bare "noqa".
